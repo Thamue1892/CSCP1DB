97b326ac82829b39400a1d33c26513d2ed830273
ThaymesCoder <thamue1892@gmail.com>
2019-07-13 04:36:26 +0200

    Implemented DayOfYear class

--- ClassDefinition/ClassDefinition.cpp
+++ ClassDefinition/ClassDefinition.cpp
@@ -2,19 +2,50 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class DayOfYear
+{
+public:
+    void output();
+    int month;
+    int day;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    DayOfYear today, birthday;
+
+    cout << "Enter today's date:\n";
+    cout << "Enter month as a number: ";
+    cin >> today.month;
+    cout << "Enter the day of the month: ";
+    cin >> today.day;
+    cout << "Enter your birthday:\n";
+    cout << "Enter month as a number: ";
+    cin >> birthday.month;
+    cout << "Enter the day of the month: ";
+    cin >> birthday.day;
+
+    cout << "Today's date is ";
+    today.output();
+
+    if (today.month == birthday.month && today.day == birthday.day)
+    {
+        cout << "Happy birthday!\n";
+    }
+    else
+    {
+        cout << "Good day!\n";
+    }
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void DayOfYear::output()
+{
+    cout << "month = " << month
+        << ", day = " << day << endl;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
