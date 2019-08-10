be0f240dd0586c0d665e205228ccdf0a049af349
ThaymesCoder <thamue1892@gmail.com>
2019-08-10 15:16:16 +0200

    Implemented Inheritance

--- IntroToInheritance/IntroToInheritance.cpp
+++ IntroToInheritance/IntroToInheritance.cpp
@@ -2,19 +2,159 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class BankAccount
+{
+public:
+	BankAccount(int dollars, int cents, double rate);
+	BankAccount(int dollars, double rate);
+	BankAccount();
+
+	void set(int dollars, int cents, double rate);
+	void set(int dollars, double rate);
+
+	void update();
+
+	double getBalance();
+
+	double getRate();
+
+	void output(ostream& outs);
+
+private:
+	double balance;
+	double interestRate;
+
+	double fraction(double percent);
+};
+
+class SavingsAccount :public BankAccount
+{
+public:
+	SavingsAccount(int dollars, int cents, double rate);
+
+	void deposit(int dollars, int cents);
+
+	void withdraw(int dollars, int cents);
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SavingsAccount account(100, 50, 5.5);
+	account.output(cout);
+	cout << endl;
+	cout << "Depositing R10.25." << endl;
+	account.deposit(10, 25);
+	account.output(cout);
+	cout << endl;
+	cout << "Withdrawing R11.80." << endl;
+	account.withdraw(11, 80);
+	account.output(cout);
+	cout << endl;
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+BankAccount::BankAccount(int dollars, int cents, double rate)
+{
+	if ((dollars < 0) || (cents < 0) || (rate < 0))
+	{
+		cout << "Illegal values for money or interest rate.\n";
+		return;
+	}
+	balance = dollars + 0.01 * cents;
+	interestRate = rate;
+}
+BankAccount::BankAccount(int dollars, double rate)
+{
+	if ((dollars < 0 || (rate < 0)))
+	{
+		cout << "Illegal values for money or interest rate.\n";
+		return;
+	}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	balance = dollars;
+	interestRate = rate;
+}
+BankAccount::BankAccount() :balance(0), interestRate(0.0)
+{
+}
+
+void BankAccount::set(int dollars, int cents, double rate)
+{
+	if ((dollars < 0) || (cents < 0) || (rate < 0))
+	{
+		cout << "Illegal values for money or interest rate.\n";
+		return;
+	}
+	balance = dollars + 0.01 * cents;
+	interestRate = rate;
+}
+
+void BankAccount::set(int dollars, double rate)
+{
+	if ((dollars < 0) || (rate < 0))
+	{
+		cout << "Illegal values for money or interest rate.\n";
+		return;
+	}
+	balance = dollars;
+	interestRate = rate;
+}
+
+void BankAccount::update()
+{
+	balance = balance + fraction(interestRate) * balance;
+}
+
+double BankAccount::fraction(double percentValue)
+{
+	return (percentValue / 100.0);
+}
+
+double BankAccount::getBalance()
+{
+	return balance;
+}
+
+double BankAccount::getRate()
+{
+	return interestRate;
+}
+
+void BankAccount::output(ostream& outs)
+{
+	outs.setf(ios::fixed);
+	outs.setf(ios::showpoint);
+	outs.precision(2);
+	outs << "Account balance R" << balance << endl;
+	outs << "Interest rate " << interestRate << "%" << endl;
+}
+
+SavingsAccount::SavingsAccount(int dollars, int cents, double rate) :
+	BankAccount(dollars, cents, rate)
+{
+}
+
+void SavingsAccount::deposit(int dollars, int cents)
+{
+	double balance = getBalance();
+	balance += dollars;
+	balance += (static_cast<double>(cents) / 100);
+	int newDollars = static_cast<int>(balance);
+	int newCents = static_cast<int>((balance - newDollars) * 100);
+	set(newDollars, newCents, getRate());
+}
+
+void SavingsAccount::withdraw(int dollars, int cents)
+{
+	double balance = getBalance();
+	balance -= dollars;
+	balance -= (static_cast<double>(cents) / 100);
+	int newDollars = static_cast<int>(balance);
+	int newCents = static_cast<int>((balance - newDollars) * 100);
+	set(newDollars, newCents, getRate());
+
+}
+
