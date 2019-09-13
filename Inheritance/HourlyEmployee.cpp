137880b27fbcaf27566e219e7629a1cee65917d9
ThaymesCoder <thamue1892@gmail.com>
2019-09-13 16:03:38 +0200

    Implemented the definition of SalariedEmployee class

--- Inheritance/HourlyEmployee.cpp
+++ Inheritance/HourlyEmployee.cpp
@@ -1,1 +1,55 @@
+#include <string>
+#include<iostream>
 #include "HourlyEmployee.h"
+
+using namespace std;
+
+namespace employeethaymes
+{
+	HourlyEmployee::HourlyEmployee() :Employee(), wageRate(0), hours(0)
+	{
+
+	}
+
+	HourlyEmployee::HourlyEmployee(string theName, string theSSN, double theWageRate, double theHours)
+		: Employee(theName, theSSN), wageRate(theWageRate), hours(theHours)
+	{
+
+	}
+
+	void HourlyEmployee::setRate(double newWageRate)
+	{
+		wageRate = newWageRate;
+	}
+
+	double HourlyEmployee::getRate() const
+	{
+		return wageRate;
+	}
+
+	void HourlyEmployee::setHours(double hoursWorked)
+	{
+		hours = hoursWorked;
+	}
+
+	double HourlyEmployee::getHours() const
+	{
+		return hours;
+	}
+
+	void HourlyEmployee::printCheck()
+	{
+		setNetPay(hours * wageRate);
+		cout << "\n__________________________________________________\n";
+		cout << "Pay to the order of " << getName() << endl;
+		cout << "The sum of " << getNetPay() << " Dollars\n";
+		cout << "\n__________________________________________________\n";
+		cout << "Check stub:NOT NEGOTIABLE\n";
+		cout << "Employee Number: " << getSSN() << endl;
+		cout << "Hourly Employee. \nHours worked: " << hours
+			 << " Rate: " << wageRate << " Pay: " << getNetPay() << endl;
+		cout << "\n__________________________________________________\n";
+
+	}
+
+}
--- Inheritance/SalariedEmployee.cpp
+++ Inheritance/SalariedEmployee.cpp
@@ -1,1 +1,55 @@
+#include <iostream>
+#include <string>
 #include "SalariedEmployee.h"
+using namespace std;
+
+namespace employeethaymes
+{
+	SalariedEmployee::SalariedEmployee() :Employee(),salary(0)
+	{
+		
+	}
+
+	SalariedEmployee::SalariedEmployee(string theName, string theSSN, double theWeeklySalary)
+		:Employee(theName,theSSN),salary(theWeeklySalary)
+	{
+		
+	}
+
+	double SalariedEmployee::getSalary() const
+	{
+		return salary;
+	}
+
+	void SalariedEmployee::setSalary(double newSalary)
+	{
+		salary=newSalary;
+	}
+
+	/*void SalariedEmployee::printCheck()
+	{
+		setNetPay(salary);
+		cout << "\n______________________________________\n";
+		cout << "Pay to the order of "<<getName() <<endl;
+		cout << "The sum of " <<getNetPay() <<" Dollars\n";
+		cout << "\n______________________________________\n";
+		cout << "Check Stub NOT NEGOTIABLE \n";
+	}*/
+
+	void SalariedEmployee::printCheck()
+	{
+		setNetPay(salary);
+		cout << "\n______________________________________\n";
+		cout << "Pay to the order of "<<getName() <<endl;
+		cout << "The sum of " <<getNetPay() <<" Dollars\n";
+		cout << "\n______________________________________\n";
+		cout << "Check Stub NOT NEGOTIABLE \n";
+		cout << "Employee Number: " << getSSN()<<endl;
+		cout <<"Salaried Employee. Regular Pay: "
+			 << salary <<endl;
+		cout << "\n______________________________________\n";
+
+	}
+
+
+}
