738bde03c45c0e4d503705e831e2dc52cc43ec3f
ThaymesCoder <thamue1892@gmail.com>
2019-09-18 02:22:53 +0200

    implemented a test project to test virtual functions.

--- VirtualFunctionsInC++/VirtualFunctionsInC++.cpp
+++ VirtualFunctionsInC++/VirtualFunctionsInC++.cpp
@@ -2,9 +2,28 @@
 //
 
 #include <iostream>
+#include "sale.h"
+#include "DiscountSale.h"
+using namespace std;
+using namespace salethaymes;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Sale simple(10.00);
+	DiscountSale discount(11.00, 10);
+
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(2);
+
+	if (discount < simple)
+	{
+		cout<<"Discounted item is cheaper.\n";
+		cout <<"Savings is $"<<simple.savings(discount)<<endl;
+	}
+	else
+		cout <<"Discounted item is not cheaper.\n";
+
+	return 0;
 }
 
